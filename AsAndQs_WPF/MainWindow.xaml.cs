e9e2c49ffa77b6e26744979b5b4f68989cc8a1f9
chajus1 <chajus1@googlemail.com>
2023-06-15 23:49:41 +0100

    hm

--- AsAndQs_WPF/MainWindow.xaml.cs
+++ AsAndQs_WPF/MainWindow.xaml.cs
@@ -16,41 +16,42 @@ namespace AsAndQs_WPF
             C c = new C();
             A a2 = new C();
             string q = "";
+
+
+            // 4 buttons
+            // entire answers shown and then the correct answer light up in green?
+
+
+
+            q += b.m1();
+            q += c.m2();
+            q += a.m3();
+
+            Console.WriteLine(q);
+
+            q = "";
+
+            q += c.m1();
+            q += c.m2();
+            q += c.m3();
+
+            Console.WriteLine(q);
+
+            q = "";
+
+            q += a.m1();
+            q += b.m2();
+            q += c.m3();
+
+            Console.WriteLine(q);
+
+            q = "";
+
+            q += a2.m1();
+            q += a2.m2();
+            q += a2.m3();
+
+            Console.WriteLine(q);
         }
-
-        // 4 buttons
-        // entire answers shown and then the correct answer light up in green?
-
-
-
-        //q += b.m1();
-        //    q += c.m2();
-        //    q += a.m3();
-
-        //    Console.WriteLine(q);
-
-        //    q = "";
-
-        //    q += c.m1();
-        //    q += c.m2();
-        //    q += c.m3();
-
-        //    Console.WriteLine(q);
-
-        //    q = "";
-
-        //    q += a.m1();
-        //    q += b.m2();
-        //    q += c.m3();
-
-        //    Console.WriteLine(q);
-
-        //    q = "";
-
-        //    q += a2.m1();
-        //    q += a2.m2();
-        //    q += a2.m3();
-
-        //    Console.WriteLine(q);
     }
 }
